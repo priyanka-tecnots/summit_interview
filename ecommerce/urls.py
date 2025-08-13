8e05b258da74dd5476fcc7c7f24dcff9802d5dca
Priyanka T <priyanka.t@tecnots.com>
2025-08-13 16:43:45 +0530

    Initial commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('api/v1/', include('apps.orders.urls')),
 ]
 
-    if settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
